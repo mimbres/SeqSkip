d0ddc96caf0c231e793a753916d5ece590ed01a2
mimbres <1ronyar@gmail.com>
2018-12-16 23:55:22 +0900

    .

--- seqskip_train_rnb1.py
+++ seqskip_train_rnb1.py
@@ -62,7 +62,7 @@ mtrain_loader = SpotifyDataloader(config_fpath=args.config,
 mval_loader  = SpotifyDataloader(config_fpath=args.config,
                                   mtrain_mode=True, # True, because we use part of trainset as testset
                                   data_sel=(99965071, 99975071),#(99965071, 124950714), # 20%를 테스트
-                                  batch_size=50,
+                                  batch_size=1024,
                                   shuffle=True) 
 
 
